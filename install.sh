64de96fa3d8292bf1e86e8f98c29e045f8b5fd8b
Pavel <39239196+Tojefin@users.noreply.github.com>
2024-03-11 11:43:41 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
+# Install Tmux
+apt install tmux
+
+# Install Tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# Copy my config file
 cp .tmux.conf ~/.tmux.conf
 
+# Set default config file
+tmux source ~/.tmux.conf
+
+# Add auto-start script
 cat .bashrc-autostart >> ~/.bashrc
